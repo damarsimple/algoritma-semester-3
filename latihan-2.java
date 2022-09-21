614aa684301a24776a387727e7f1c46712d5f906
Damar Albaribin <damaralbaribin@gmail.com>
2022-09-21 09:21:19 +0700

    feat : added latihan x star

---
+++ latihan-2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class latihan2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Masukkan n : ");
+
+        int n = scan.nextInt();
+
+        // draw x star
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                bool eval = i == j || i + j == n - 1;
+
+                if (eval) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+}
