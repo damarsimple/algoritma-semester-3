fdb86d2b8a55e18ab68f31afcfb64ac1d1106376
Damar Albaribin <damaralbaribin@gmail.com>
2022-09-21 10:17:33 +0700

    feat : correct project structure

--- latihan-2.java
+++ latihan-2.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class latihan2 {
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
@@ -9,30 +10,11 @@ public class latihan2 {
 
         int n = scan.nextInt();
 
-        // draw x star
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-
-                boolean render = i == j || i + j == n - 1;
-
-                if (render) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-
-            }
-            System.out.println();
+        for (int i = 1; i <= 2 * n; i++) {
+            System.out.printf("%d ", i);
         }
 
+        System.out.println();
     }
-}
 
-// Here's what the above main function is doing:
-// 1. First it will ask the user to enter a number.
-// 2. Then it will draw an x star with the width and height of the entered
-// number.
-// 3. For each row and column, it will draw a star if the row equals the column
-// or the sum of the row and column equals the width minus one.
-// 4. If not, it will draw a space.
+}
