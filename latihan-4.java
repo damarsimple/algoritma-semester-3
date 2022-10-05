d35c91c07aa96265602733ee3f6c9babfa184637
Damar Albaribin <damaralbaribin@gmail.com>
2022-10-05 09:26:46 +0700

    feat : latihan 4 png

--- latihan-4.java
+++
@@ -1,9 +0,0 @@
-import com.damar.uts.Mahasiswa;
-
-public class latihan4 {
-
-    public static void main(String[] args) {
-        Mahasiswa mahasiswa = new Mahasiswa("Damar", "1234567890");
-    }
-
-}
